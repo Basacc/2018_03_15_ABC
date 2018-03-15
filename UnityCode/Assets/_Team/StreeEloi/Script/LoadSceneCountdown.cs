93dceeaaf91fe88d51955cde4fed99f95b03429a
Eloi Strée <eloi@jams.center>
2018-03-15 13:12:36 +0100

    Add script to switch scenes and the logo of technobel

---
+++ UnityCode/Assets/_Team/StreeEloi/Script/LoadSceneCountdown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneCountdown : MonoBehaviour {
+
+    public string _sceneNameToLoad= "DefaultScene";
+    public float _loadNextSceneAfter = 5;
+
+    public void Start()
+    {
+        Invoke("LoadNextScene", _loadNextSceneAfter);
+    }
+    public void LoadNextScene() {
+        SceneManager.LoadSceneAsync(_sceneNameToLoad);
+    }
+
+}
